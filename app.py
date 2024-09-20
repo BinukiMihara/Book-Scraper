a29c1408304e0746d60a973f3f7b2b65f9d4d0d8
Binuki Mihara <133636115+BinukiMihara@users.noreply.github.com>
2024-09-20 11:17:18 +0530

    Extract all the book details successfully

--- app.py
+++ app.py
@@ -18,7 +18,19 @@ try:
     soup = BeautifulSoup(source.text, 'html.parser')
 
     books = soup.find('ol', class_ = "row").find_all('li', class_ = "col-xs-6 col-sm-4 col-md-3 col-lg-3")
-    print(len(books))
+
+    for book in books:
+        name = book.find('h3').a.text
+        #rating = book.find('p', class_ = "star-rating Three")['class'][1]
+        rating_tag = book.find('p', class_="star-rating")
+        if rating_tag:
+            rating = rating_tag['class'][1]  # Second class contains the rating (e.g., "Three", "Four")
+        else:
+            rating = "No rating"
+        price = book.find('div', class_ = "product_price").p.get_text(strip=True).strip('£')[1:]
+        stock = book.find('p', class_ = "instock availability").get_text(strip=True).strip('""')
+        print(name, rating, price, stock)
+        
     
 except Exception as e:
     print(f"Error: {e}")
